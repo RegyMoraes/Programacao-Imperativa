993fbd5e7d3eb8fb719561238f4d5b137a4ca242
Regy <87830736+RegyMoraes@users.noreply.github.com>
2021-11-01 13:23:45 -0300

    Add files via upload
    calculadora 2

---
+++ calculadora2.js
@@ -0,0 +1,30 @@
+// calculadora2
+//Função adicionar
+function adicionar (a, b) {
+   return a + b;
+}
+   console.log (adicionar (3,3)); 
+
+   //Função subtrair
+function subtrair (a, b) {
+   return a - b;
+}
+   console.log (subtrair(3,3)); 
+
+    //Função Multiplicar
+function multiplicar (a, b) {
+    return a * b;
+ }
+    console.log (multiplicar(3,3)); 
+
+     //Função Dividir
+function dividir (a, b) {
+    return a / b;
+ }
+    console.log (dividir(3,3)); 
+ 
+    console.log("Teste de Operações");
+    console.log(adicionar (1,100)); 
+    console.log(subtrair (100,4)); 
+    console.log(multiplicar(50,2)); 
+    console.log(dividir(100,2)); 
