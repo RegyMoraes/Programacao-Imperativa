216c427605fdebff3548347ba55472bdca6c062b
Regy <87830736+RegyMoraes@users.noreply.github.com>
2021-11-01 11:45:33 -0300

    Calculadora1
    versão 1 calculadora

---
+++ calculadora.js
@@ -0,0 +1,25 @@
+// Função adicionar
+function adicionar (a, b) {
+   return a + b;
+}
+   console.log (adicionar (3,3)); 
+
+   //Função subtrair
+function subtrair (a, b) {
+   return a - b;
+}
+   console.log (subtrair(3,3)); 
+
+    //Função Multiplicar
+function multiplicar (a, b) {
+    return a * b;
+ }
+    console.log (multiplicar(3,3)); 
+
+     //Função Dividir
+function dividir (a, b) {
+    return a / b;
+ }
+    console.log (dividir(3,3)); 
+ 
+ 
